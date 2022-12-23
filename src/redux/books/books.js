ffce07b68955e831167f2c761f62f4d6817d4d24
umair-rao <umairmuqeem@gmail.com>
2022-12-23 13:09:35 +0500

    Book removed using filter

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,25 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
 export const addBook = (book) => ({
-  type: 'add',
+  type: ADD_BOOK,
   payload: book,
 });
 
-export const removeBook = (book) => ({
-  type: 'remove',
-  payload: book,
+export const removeBook = (state) => ({
+  type: REMOVE_BOOK,
+  books: state.books.filter((book) => book.id !== state.book.id),
 });
+
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return [...state, action.books];
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
